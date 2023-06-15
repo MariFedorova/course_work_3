937ea0ab945623ac670737b2f298797612c8b62f
Mariia Fedorova <000maria.fed@gmail.com>
2023-06-15 21:10:11 +0300

    add sort operations

--- src/funcs.py
+++ src/funcs.py
@@ -6,3 +6,10 @@ def load_operations(filename):
     with open(filename, "r", encoding="utf-8") as json_file:
         data = json.load(json_file)
         return data
+
+def sort_operations(state, dict_):
+    executed = []
+    for item in dict_:
+        if item.get("state") == state:
+            executed.append(item)
+    return executed
